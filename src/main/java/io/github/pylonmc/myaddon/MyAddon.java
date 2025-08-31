33a2e623cf800bfa2325cbdbe0187e6e42921aab
LordIdra <percy.baker.0@gmail.com>
2025-08-31 22:05:29 +0100

    bump versions + small fixes

---
+++ src/main/java/io/github/pylonmc/myaddon/MyAddon.java
@@ -0,0 +1,41 @@
+package io.github.pylonmc.myaddon;
+
+import io.github.pylonmc.pylon.core.addon.PylonAddon;
+import lombok.Getter;
+import org.bukkit.Material;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Locale;
+import java.util.Set;
+
+
+public class MyAddon extends JavaPlugin implements PylonAddon {
+
+    // The `instance` field stores the instance of the addon (there's only ever one)
+    @Getter private static MyAddon instance;
+
+    // Called when our plugin is enabled
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        // Every Pylon addon must call this BEFORE doing anything Pylon-related
+        registerWithPylon();
+    }
+
+    @Override
+    public @NotNull JavaPlugin getJavaPlugin() {
+        return instance;
+    }
+
+    @Override
+    public @NotNull Set<@NotNull Locale> getLanguages() {
+        return Set.of(Locale.ENGLISH);
+    }
+
+    @Override
+    public @NotNull Material getMaterial() {
+        return Material.DEAD_BUSH;
+    }
+}
